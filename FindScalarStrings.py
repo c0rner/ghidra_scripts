9bbe53db0675309bba4e62b16f44bfe05f7c8f9f
Martin Akesson <2602099+c0rner@users.noreply.github.com>
2022-01-23 21:20:10 +0100

    Add script for findings strings in scalars

---
+++ FindScalarStrings.py
@@ -0,0 +1,84 @@
+# Annotate scalar values that are valid ASCII strings with a EOL comment.
+#
+# Supported architectures
+# -----------------------
+# x86 (immediate values)
+# 
+# @author https://github.com/c0rner
+# @category Strings
+# @keybinding
+# @menupath
+# @toolbar
+
+from pprint import pprint
+from ghidra.program.model.lang import OperandType
+
+def isUTF8(data):
+    """ Verify data is valid UTF-8
+    
+    Args:
+        data (b[]): Byte array
+        
+    Returns:
+        bool: Set to True if valid UTF-8
+    """
+    try:
+        data.tostring().decode('utf-8')
+        return True
+    except:
+        return False
+    return True
+
+def isAscii(data):
+    """ Verify data is printable/readable ASCII
+
+    Args:
+        data (b[]): Byte array
+
+    Returns:
+        bool: Set to True if ASCII string
+    """
+    for b in data:
+        if b < 0x20 or b > 0x7e: # Printable range
+            return False
+    return True
+
+def x86_find_scalar_string(inst):
+    """ Find strings in x86 immediate values
+    
+    Args:
+        inst (Instruction): Ghidra Instruction to inspect
+        
+    Returns:
+        str: String if found else None
+    """
+    oper = inst.getMnemonicString()
+    if oper == "MOV" or oper == "CMP":
+        if OperandType.isScalar(inst.getOperandType(1)):
+            data = inst.getOpObjects(1)[0].byteArrayValue()
+            # Check immediate value for readable ASCII
+            if len(data) > 1 and isAscii(data):
+                # Reverse and convert to string
+                return data[::-1].tostring()
+    return None
+
+
+b_text = getMemoryBlock(".text")
+listing = currentProgram.getListing()
+
+langId = currentProgram.getLanguageID().getIdAsString()
+if not langId.startswith("x86"):
+    raise Exception("Unsupported architecture (%s)" % langId)
+
+update = 0 # Counter for monitor updates
+monitor.initialize(listing.getNumInstructions())
+for inst in listing.getInstructions(b_text.getStart(), True):
+    update += 1
+    if update >= 1000:
+        update = 0
+        monitor.checkCanceled()
+        monitor.incrementProgress(1000)
+    str = x86_find_scalar_string(inst)
+    if str is not None:
+        #print("%s -> %s" % (inst.getAddress(), str))
+        inst.setComment(inst.EOL_COMMENT, "\"%s\"" % str)
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ghidra_scripts
+
+### FindScalarStrings.py
+Annotate scalar values that are valid ASCII strings with a EOL comment.
+<details><summary>Supported architecture</summary>
+
+### x86
+Implemented for immediate values in CMP and MOV. Caveat: Currently any __0x0d__ (_carriage return_)  or __0x0a__ (_line feed_)  in imm value will discard string.
+
+</details>
